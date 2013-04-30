54199b08b2df8b65c7a081238e5bc60df5176af0
Tal Rotbart <redbeard@gmail.com>
2013-04-30 15:46:06 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soylent::Application.config.secret_key_base = '8112747eca4f6b332e84d2d86d20cee342e280164e8550d85f445e4728652b82b289c1d6940873c6ac5297bd12763eb3d395b055fcc3f77e2e977f1c9bbeeeb8'
